be65f18079e31bdcfdc209688ffbe593ac2be1e7
tx-ares <twinadanz@gmail.com>
2024-12-16 09:44:17 -0600

    set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import "./App.css";
-
-import Login from "./components/Login/Login";
+import { Route, Routes } from 'react-router';
+import './App.css';
+import LoginPage from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+	return (
+		<Routes>
+			<Route
+				path='*'
+				element={<LoginPage />}
+			/>
+
+			<Route
+				path='/login'
+				element={<LoginPage />}
+			/>
+			<Route
+				path='/dashboard'
+				element={<Dashboard />}
+			/>
+		</Routes>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import theme from "./Theme.tsx";
-import App from "./App.tsx";
-import { ThemeProvider } from "@mui/material";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import theme from './Theme.tsx';
+import App from './App.tsx';
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
+	</StrictMode>
 );
